8122fe88e11ef85b6c628efe9559a850d7942690
Chris Puncekar <cpmochagames@gmail.com>
2024-05-20 18:33:11 -0500

    optional javascript file
    optional javascript file (js)

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+
+// JavaScript functionality can be added here as needed
+// Currently, there are no interactive elements that require JavaScript
+console.log('Eco-Friendly Cleaning Guide Loaded');
